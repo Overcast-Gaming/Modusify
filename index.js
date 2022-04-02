7e5fbdbc5d51e62c544172cd01f5e4ebadbe898b
Overcast <57546895+Overcast-Gaming@users.noreply.github.com>
2022-04-02 13:34:12 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = function modusify(string = 'The quick brown fox jumps over the lazy dog') {
+    if (typeof string !== "string") throw new TypeError("String required! Received: " + typeof string);
+    string = string.split(' ');
+    const times = Math.round((Math.random() * (Math.ceil(string.length/5) - 1) + 1));
+    for (i=0;i<times;i++) {
+        const j = Math.floor(Math.random() * string.length);
+        string.splice(j, 0, '...');
+    }
+    return string.join(' ').replaceAll(' ...','...')
+};
